7112b581f7fe95ba7d1caebb9640cc9625c107ad
Jpdm1203 <119541106+Jpdm1203@users.noreply.github.com>
2023-10-21 00:12:16 -0600

    Update

--- app/week5/page.js
+++ app/week5/page.js
@@ -1,9 +1,11 @@
-import NewItem from "./new-item";
+
+import ItemList from "./item-list";
 
 export default function Page() {
-  return (
-    <main>
-      <NewItem />
-    </main>
-  );
+    return (
+        <main className="mx-4">
+            <h1 className="text-3xl font-bold my-4">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
 }
