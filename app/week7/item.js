e90ecc0827059c50ba0afed87f040ad03923524f
jpm1203 <johnpatrick.mendoza@edu.sait.ca>
2023-11-30 12:33:10 -0700

    check

--- app/week7/item.js
+++ app/week7/item.js
@@ -1,10 +1,12 @@
+import React from 'react';
+
 export default function Item ({ name, quantity, category }) {
-    return (
-      /* for items.json layout */
-      <div className="border border-sky-500 rounded-lg shadow-md bg-transparent-800 w-full max-w-md m-2 p-2">
-        <h1 className="text-xl font-bold">{name}</h1>
-        <p>Quantity: {quantity}</p>
-        <p>Category: {category}</p>
-      </div>
-    );
-  }
+  return (
+    /* for items.json layout */
+    <div className="border border-sky-500 rounded-lg shadow-md bg-transparent-800 w-full max-w-md m-2 p-2">
+      <h1 className="text-xl font-bold">{name}</h1>
+      <p>Quantity: {quantity}</p>
+      <p>Category: {category}</p>
+    </div>
+  );
+}
